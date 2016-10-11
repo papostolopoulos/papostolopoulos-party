2c51ce99c6b862fe061bc1cf0e5f811c7fd0e7bd
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2016-10-11 16:21:29 -0700

    Add the create subcommand

--- guests.js
+++ guests.js
@@ -17,6 +17,30 @@ if (cmd === "read") {
     console.log(guests);
   });
 }
+else if (cmd === "create") {
+  fs.readFile(guestsPath, "utf8", function (readErr, data) {
+    if (readErr) {
+      throw readErr;
+    }
+    var guests = JSON.parse(data);
+    var guest = process.argv[3];
+
+    if (!guest) {
+      console.error(`Usage: ${node} ${file} ${cmd} GUEST`);
+      process.exit(1);
+    }
+    guests.push(guest);
+
+    var guestsJSON = JSON.stringify(guests);
+
+    fs.writeFile(guestsPath, guestsJSON, function(writeErr){
+      if (writeErr) {
+        throw writeErr;
+      }
+      console.log(guest);
+    });
+  });
+}
 else {
   console.error(`Usage: ${node} ${file} read`);
   process.exit(1);
