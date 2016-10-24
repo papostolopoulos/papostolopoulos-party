85ec7a148c375e350307093c01cbd82bec8df43f
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2016-10-24 14:51:12 -0700

    Add and Express HTTP server

--- serverExpress.js
+++ serverExpress.js
@@ -1,20 +1,49 @@
-"use strict";
+'use strict';
 
-var express = require("express");
+var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 
-app.get("/guests", function(req, res) {
-  var guests = ["Mary", "Don"];
+app.use(function(req, res) {
+  var guests = ['Mary', 'Don'];
   res.send(guests);
 });
 
-app.use(function(req, res) {
-  res.sendStatus(404);
-});
-
 app.listen(port, function() {
-  console.log("Listening on port ", port);
+  console.log('Listening on port', port);
 });
+//
+// 'use strict';
+//
+// var fs = require('fs');
+// var path = require('path');
+// var guestsPath = path.join(__dirname, 'guests.json');
+//
+// var express = require('express');
+// var app = express();
+// var port = process.env.PORT || 8000;
+//
+// app.disable('x-powered-by');
+//
+// app.get('/guests', function(req, res) {
+//   fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
+//     if (err) {
+//       console.error(err.stack);
+//       return res.sendStatus(500);
+//     }
+//
+//     var guests = JSON.parse(guestsJSON);
+//
+//     res.send(guests);
+//   });
+// });
+//
+// app.use(function(req, res) {
+//   res.sendStatus(404);
+// });
+//
+// app.listen(port, function() {
+//   console.log('Listening on port', port);
+// });
