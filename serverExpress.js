3d5b786d147b47e572b4ca57a38e3a342eca549a
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2016-10-24 22:53:18 -0700

    Send all guest records from the database

--- serverExpress.js
+++ serverExpress.js
@@ -1,19 +1,47 @@
 'use strict';
 
+var fs = require("fs");
+var path = require("path");
+var guestsPath = path.join(__dirname, "guests.json");
+
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
 app.disable('x-powered-by');
 
+app.get("/guests", function(req, res) {
+  fs.readFile(guestsPath, "utf8", function (err, guestsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+    
+    var guests = JSON.parse(guestsJSON);
+    res.send(guests);
+  });
+});
+
 app.use(function(req, res) {
-  var guests = ['Mary', 'Don'];
-  res.send(guests);
+  res.sendStatus(404);
 });
 
 app.listen(port, function() {
   console.log('Listening on port', port);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 //
 // 'use strict';
 //
