1bb4d1b3c2e3fcfb470e26e887566c1977b6d167
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2016-10-25 15:30:14 -0700

    Add body-parser middleware

--- serverExpress.js
+++ serverExpress.js
@@ -1,37 +1,88 @@
 'use strict';
 
-var fs = require("fs");
-var path = require("path");
-var guestsPath = path.join(__dirname, "guests.json");
+var fs = require('fs');
+var path = require('path');
+var guestsPath = path.join(__dirname, 'guests.json');
 
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-var morgan = require("morgan");
+var morgan = require('morgan');
 
 app.disable('x-powered-by');
-app.use(morgan("short"));
+app.use(morgan('short'));
 
-app.get("/guests", function(req, res) {
-  fs.readFile(guestsPath, "utf8", function(err, guestsJSON) {
+app.use(function(req, res, next) {
+  var bodyJSON = '';
+
+  req.on('data', function(chunk) {
+    bodyJSON += chunk.toString();
+  });
+
+  req.on('end', function() {
+    var body;
+
+    if (bodyJSON !== '') {
+      body = JSON.parse(bodyJSON);
+    }
+
+    req.body = body;
+
+    next();
+  });
+});
+
+app.get('/guests', function(req, res) {
+  fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
     if (err) {
       console.error(err.stack);
       return res.sendStatus(500);
     }
+
     var guests = JSON.parse(guestsJSON);
+
     res.send(guests);
   });
 });
 
-app.get('guests/:id', function (req, res) {
-  console.log("inside");
-  fs.readFile(guestsPath, "utf8", function (err, guestsJSON) {
+app.post('/guests', function(req, res) {
+  fs.readFile(guestsPath, 'utf8', function(readErr, guestsJSON) {
+    if (readErr) {
+      console.error(readErr.stack);
+      return res.sendStatus(500);
+    }
+
+    var guests = JSON.parse(guestsJSON);
+    var guest = req.body.name;
+
+    if (!guest) {
+      return res.sendStatus(400);
+    }
+
+    guests.push(guest);
+
+    var newGuestsJSON = JSON.stringify(guests);
+
+    fs.writeFile(guestsPath, newGuestsJSON, function(writeErr) {
+      if (writeErr) {
+        console.error(writeErr.stack);
+        return res.sendStatus(500);
+      }
+
+      res.set('Content-Type', 'text/plain');
+      res.send(guest);
+    });
+  });
+});
+
+app.get('/guests/:id', function(req, res) {
+  fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
     if (err) {
       console.error(err.stack);
       return res.sendStatus(500);
     }
-    console.log(guests.length);
+
     var id = Number.parseInt(req.params.id);
     var guests = JSON.parse(guestsJSON);
 
@@ -39,7 +90,7 @@ app.get('guests/:id', function (req, res) {
       return res.sendStatus(404);
     }
 
-    res.set("Content-Type", "text/plain");
+    res.set('Content-Type', 'text/plain');
     res.send(guests[id]);
   });
 });
@@ -49,5 +100,5 @@ app.use(function(req, res) {
 });
 
 app.listen(port, function() {
-  console.log('Listening on port', port, "and for express party practice");
+  console.log('Listening on port', port);
 });
