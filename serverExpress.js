d530050ec45efd9841134606bfb89b3790fd9e9c
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2016-11-04 13:22:13 -0700

    Add update REST action

--- serverExpress.js
+++ serverExpress.js
@@ -1,3 +1,5 @@
+//--------Morgan included---------
+
 'use strict';
 
 var fs = require('fs');
@@ -9,29 +11,11 @@ var app = express();
 var port = process.env.PORT || 8000;
 
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 app.disable('x-powered-by');
 app.use(morgan('short'));
-
-app.use(function(req, res, next) {
-  var bodyJSON = '';
-
-  req.on('data', function(chunk) {
-    bodyJSON += chunk.toString();
-  });
-
-  req.on('end', function() {
-    var body;
-
-    if (bodyJSON !== '') {
-      body = JSON.parse(bodyJSON);
-    }
-
-    req.body = body;
-
-    next();
-  });
-});
+app.use(bodyParser.json());
 
 app.get('/guests', function(req, res) {
   fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
@@ -46,10 +30,29 @@ app.get('/guests', function(req, res) {
   });
 });
 
+app.get('/guests/:id', function(req, res) {
+  fs.readFile(guestsPath, 'utf8', function(err, newGuestsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var id = Number.parseInt(req.params.id);
+    var guests = JSON.parse(newGuestsJSON);
+
+    if (id < 0 || id >= guests.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    res.set('Content-Type', 'text/plain');
+    res.send(guests[id]);
+  });
+});
+
 app.post('/guests', function(req, res) {
   fs.readFile(guestsPath, 'utf8', function(readErr, guestsJSON) {
     if (readErr) {
-      console.error(readErr.stack);
+      console.error(err.stack);
       return res.sendStatus(500);
     }
 
@@ -76,10 +79,10 @@ app.post('/guests', function(req, res) {
   });
 });
 
-app.get('/guests/:id', function(req, res) {
-  fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
-    if (err) {
-      console.error(err.stack);
+app.put('/guests/:id', function(req, res) {
+  fs.readFile(guestsPath, 'utf8', function(readErr, guestsJSON) {
+    if (readErr) {
+      console.error(readErr.stack);
       return res.sendStatus(500);
     }
 
@@ -90,8 +93,25 @@ app.get('/guests/:id', function(req, res) {
       return res.sendStatus(404);
     }
 
-    res.set('Content-Type', 'text/plain');
-    res.send(guests[id]);
+    var guest = req.body.name;
+
+    if (!guest) {
+      return res.sendStatus(400);
+    }
+
+    guests[id] = guest;
+
+    var newGuestsJSON = JSON.stringify(guests);
+
+    fs.writeFile(guestsPath, newGuestsJSON, function(writeErr) {
+      if (writeErr) {
+        console.error(writeErr.stack);
+        return res.sendStatus(500);
+      }
+
+      res.set('Content-Type', 'text/plain');
+      res.send(guest);
+    });
   });
 });
 
@@ -100,5 +120,65 @@ app.use(function(req, res) {
 });
 
 app.listen(port, function() {
-  console.log('Listening on port', port);
+  console.log('Listening on port', port, "and for project 'Party'");
 });
+
+//---WITHOUT morgan
+// 'use strict';
+//
+// var fs = require('fs');
+// var path = require('path');
+// var guestsPath = path.join(__dirname, 'guests.json');
+//
+// var express = require('express');
+// var app = express();
+// var port = process.env.PORT || 8000;
+//
+// app.disable('x-powered-by');
+//
+// app.use(function(req, res, next) {
+//   var start = new Date();
+//   next();
+//   var end = new Date();
+//   console.log(req.method, req.url, res.statusCode, end - start, 'ms');
+// });
+//
+// app.get('/guests', function(req, res) {
+//   fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
+//     if (err) {
+//       console.error(err.stack);
+//       return res.sendStatus(500);
+//     }
+//
+//     var guests = JSON.parse(guestsJSON);
+//
+//     res.send(guests);
+//   });
+// });
+//
+// app.get('/guests/:id', function(req, res) {
+//   fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
+//     if (err) {
+//       console.error(err.stack);
+//       return res.sendStatus(500);
+//     }
+//
+//     var id = Number.parseInt(req.params.id);
+//     var guests = JSON.parse(guestsJSON);
+//
+//     if (id < 0 || id >= guests.length || Number.isNaN(id)) {
+//       return res.sendStatus(404);
+//     }
+//
+//     res.set('Content-Type', 'text/plain');
+//     res.send(guests[id]);
+//   });
+// });
+//
+// app.use(function(req, res) {
+//   res.sendStatus(404);
+// });
+//
+// app.listen(port, function() {
+//   console.log('Listening on port', port, 'and for project "Party" for express server');
+// });
